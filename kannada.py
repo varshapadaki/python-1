a62d8065137218fb80faecd56155034f354d8daf
Ruhetarannum <167198286+Ruhetarannum@users.noreply.github.com>
2024-04-24 09:21:39 +0530

    Add files via upload

---
+++ kannada.py
@@ -0,0 +1,2 @@
+print('name in kannada:',(chr(3248)+chr(3266)+chr(3257)+chr(3263)))#unicode decimal values
+
---
+++ namepattern.py
@@ -0,0 +1,10 @@
+'''n=str(input('enter your name:'))# X pattern name
+l=len(n)
+for i in range (l):
+    for j in range (l):
+        if(i==j or i+j==l-1):
+            print(n[i],end=' ')
+        else:
+            print(' ',end=' ')
+    print()'''
+
